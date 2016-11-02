70ae7cf6ff1e2cc96e3741e8a2925177a86c0384
Brad Suchoski <brad.suchoski@gmail.com>
2016-11-02 09:32:49 -0400

    Added pdf project report and build directory with executable from my Arch linux desktop

---
+++ build/CMakeFiles/morph.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/morph.dir/morph_generated_yuv2rgb.cu.o"
+  "CMakeFiles/morph.dir/morph_generated_morph.cu.o"
+  "CMakeFiles/morph.dir/main.cpp.o"
+  "morph.pdb"
+  "morph"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/morph.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
